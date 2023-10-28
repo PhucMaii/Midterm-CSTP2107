8aee23c8a40fd5106118d904f4d2665049a9a7ae
Thien Phuc Mai <000461754@student.vcc.ca>
2023-10-28 11:53:46 -0700

    Login Page UI and Firebase linked

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,8 +17,11 @@ const firebaseConfig = {
   appId: "1:286839159181:web:7599800d684d4cb2e665c4",
   measurementId: "G-VQME4J7Z7W"
 };
-console.log(firebaseConfig.apiKey)
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const db = getFirestore();
+const storage = getStorage();
+
+export { auth, db, storage }
---
+++ src/pages/LoginPage/styles.jsx
@@ -0,0 +1,14 @@
+import { Form } from "antd";
+import styled from "styled-components";
+
+const FormStyled = styled(Form)`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 50px;
+    border-radius: 20px;
+    gap: 10px;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`
+
+export {FormStyled}
