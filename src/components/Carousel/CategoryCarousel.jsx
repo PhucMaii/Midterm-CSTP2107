e086fb6ab23ae52b98fdbdff29d865cd8a36c92a
PhucMaii <maithienphuc0102@gmail.com>
2023-10-28 18:23:03 -0700

    Complete Home Page

--- src/components/Carousel/CategoryCarousel.jsx
+++ src/components/Carousel/CategoryCarousel.jsx
@@ -1,70 +1,52 @@
+import { useState } from "react";
 import { Col, Row } from "antd";
-import { CategoryContainerStyled, PlayButtonStyled } from "./styles";
-import { TitleStyled } from "../Header/styles";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { categoriesEnum } from "../../utils/constant";
+import { CategoryContainerStyled, PlayButtonStyled } from "./styles";
+import { TitleStyled } from "./styles";
+import { useNavigate } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 export default function CategoryCarousel() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const navigate = useNavigate();
+
     return (
         <Swiper
-        style={{top: '-15vh', marginLeft: '20px',}}
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={1.5}
-        onSwiper={(swiper) => console.log(swiper.activeIndex)}
-        onSlideChange={(e) => console.log(e.activeIndex)}
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+            slidesPerView={1.5}
+            style={{top: '-15vh', marginLeft: '20px'}}
         >
-            <SwiperSlide>
-                <CategoryContainerStyled $activeIndex={true}>
-                    <Row>
-                        <Col span={24}>
-                            <TitleStyled level={3}>Linux</TitleStyled>
-                        </Col>
-                        <Col>
-                            <PlayButtonStyled $activeIndex={true} twoToneColor='rgba(0, 0, 0, 0)'/>
-                        </Col>
-                    </Row>
-                </CategoryContainerStyled>
-            </SwiperSlide>
-            <SwiperSlide $activeIndex={1}>
-            <CategoryContainerStyled>
-                    <Row>
-                        <Col span={24}>
-                            <TitleStyled level={3}>Linux</TitleStyled>
-                        </Col>
-                        <Col>
-                            <PlayButtonStyled twoToneColor='rgba(0, 0, 0, 0)'/>
-                        </Col>
-                    </Row>
-                </CategoryContainerStyled>
-            </SwiperSlide>
-            <SwiperSlide $activeIndex={2}>
-            <CategoryContainerStyled>
-                    <Row>
-                        <Col span={24}>
-                            <TitleStyled level={3}>Linux</TitleStyled>
-                        </Col>
-                        <Col>
-                            <PlayButtonStyled twoToneColor='rgba(0, 0, 0, 0)'/>
-                        </Col>
-                    </Row>
-                </CategoryContainerStyled>
-            </SwiperSlide>
-            <SwiperSlide $activeIndex={3}>
-            <CategoryContainerStyled>
-                    <Row>
-                        <Col span={24}>
-                            <TitleStyled level={3}>Linux</TitleStyled>
-                        </Col>
-                        <Col>
-                            <PlayButtonStyled twoToneColor='rgba(0, 0, 0, 0)'/>
-                        </Col>
-                    </Row>
-                </CategoryContainerStyled>
-            </SwiperSlide>
+            {
+                categoriesEnum.map((category, index) => {
+                    return (
+                    <SwiperSlide key={index}>
+                        <CategoryContainerStyled
+                            $activeIndex={activeIndex === index}
+                            $image={category.image}
+                        >
+                            <Row>
+                                <Col span={24}>
+                                    <TitleStyled level={3}>{category.name}</TitleStyled>
+                                </Col>
+                                <Col>
+                                    <PlayButtonStyled 
+                                        onClick={() => navigate(`/home/${category.name}`)}
+                                        $activeIndex={activeIndex === index} 
+                                        twoToneColor='rgba(0, 0, 0, 0)'
+                                    />
+                                </Col>
+                            </Row>
+                        </CategoryContainerStyled>
+                    </SwiperSlide>
+                    )
+                })
+            }
       </Swiper>
     )
 }
--- src/components/QuizCategoryCard/QuizCategoryCard.jsx
+++ src/components/QuizCategoryCard/QuizCategoryCard.jsx
@@ -1,24 +1,47 @@
-import { PlayCircleFilled } from "@ant-design/icons";
-import { Col, Row, Typography } from "antd";
-import { ContainerStyled } from "./styles";
+import PropTypes from 'prop-types';
+import { Col, Progress, Row, Space, Typography } from "antd";
+import { CardContainer, ContainerStyled, ImageStyled } from "./styles";
 
-export default function QuizCategoryCard() {
+export default function QuizCategoryCard({quiz}) {
     const { Title } = Typography;
     return (
         <ContainerStyled>
-            <Row>
-                <Col span={24}>
-                    <Title level={4}>Categories Name</Title>
-                </Col>   
-            </Row>
-            <Row style={{alignItems: 'center'}}>
-                <Col span={2}>
-                    <PlayCircleFilled size="small"/>
+            <CardContainer 
+                align="middle"
+                gutter={[8, 0]} 
+            >
+                <Col span={8} style={{ top: '15px'}}>
+                    <ImageStyled src={quiz.quizInfo.image}/>
                 </Col>
-                <Col span={22}>
-                    10 questions
+                <Col span={12}>
+                    <Row>
+                        <Col span={24}>
+                            <Title level={4}>{quiz.quizName}</Title>
+                        </Col>   
+                    </Row>
+                    <Row align="middle">
+                        <Col span={22}>
+                            {quiz.isDone ? "You completed the quiz" : "Let's finish the quiz"}
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
+                <Col span={4} style={{ top: '15px'}}>
+                    <Space wrap>
+                        <Progress 
+                            format={() => `${quiz.currentRecord} / ${quiz.totalQuizzes} `} 
+                            percent={quiz.currentRecord / quiz.totalQuizzes * 100} 
+                            size="small" 
+                            strokeWidth={7} 
+                            type="circle" 
+                        />
+                    </Space>
+
+                </Col>
+            </CardContainer>     
         </ContainerStyled>
     )
+}
+
+QuizCategoryCard.propTypes = {
+    quiz: PropTypes.object.isRequired
 }
